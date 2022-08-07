1f57273e1bead120d5d4493389e42a8a4ca0ef31
Tatsiana Vaitsekhouskaya <t.vaitsekhouskaya@rozumsoft.by>
2022-08-07 15:42:48 +0300

    added the check of filled data

--- src/test/java/com/demoqa/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -16,9 +17,7 @@ public class StudentRegistrationForm {
     static void congigure() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-
     }
-
     @Test
     void fillForm() {
         open("/automation-practice-form");
@@ -26,25 +25,22 @@ public class StudentRegistrationForm {
         $("#lastName").setValue("Vai");
         $("#userEmail").setValue("tanyavai@test.com");
         $("[for=gender-radio-2]").click();
-        $("#userNumber").setValue("3751231236");
+        $("#userNumber").setValue("1234567897");
         $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $("#dateOfBirthInput").sendKeys("01.02.2001");
         $("#subjectsInput").setValue("Maths").pressEnter();
         $("[for=\"hobbies-checkbox-2\"]").click();
          $("#uploadPicture").uploadFile(new File("src/test/java/../resources/PC.png"));
-         $("#currentAddress").setValue("Minsk Cool street");
+         $("#currentAddress").setValue("Minsk, Vostochnaya street");
          executeJavaScript("$('footer').remove()");
          executeJavaScript("$('#fixedban').remove()");
          $("#state").click();
          $(byText("NCR")).click();
          $("#city").click();
-         $(byText("Delhi")).click();
+         $(byText("Gurgaon")).click();
          $("#submit").click();
-
-
-
-
-
-        sleep(4000);
+         $(".modal-body").shouldHave(text("Tanya Vai"), text("tanyavai@test.com"), text("Female"),
+                 text("1234567897"), text("02 January,2001"), text("Maths"), text("Reading"), text("PC.png"),
+                 text("Minsk, Vostochnaya street"), text("NCR Gurgaon"));
     }
 }
