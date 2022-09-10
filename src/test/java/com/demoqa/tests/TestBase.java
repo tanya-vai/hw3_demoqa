49629ab23be2b2a97a9a3c30507c37e2b4e85bde
Tatsiana Vaitsekhouskaya <t.vaitsekhouskaya@rozumsoft.by>
2022-09-10 22:44:08 +0300

    added remote selenoid

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -12,6 +12,7 @@ public class TestBase {
     static void configure() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote ="https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
 
     }
