fd5f1584527ab8d0f43ed4f3ca373b46fadef00b
Tatsiana Vaitsekhouskaya <t.vaitsekhouskaya@rozumsoft.by>
2022-09-17 22:20:41 +0300

    added System.getProperty

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -16,14 +16,18 @@ public class TestBase {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
 
-        Configuration.browserCapabilities = capabilities;
+        if(System.getProperty("remote_url")!=null) {
+            Configuration.remote =System.getProperty("remote_url");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+        }
+
+        Configuration.browser = System.getProperty("browser_name", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote ="https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
 
     }
 
